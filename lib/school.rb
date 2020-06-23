298c296eb97cd883db8b7c4f6d782ba1263e0a30
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-23 20:21:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+  attr_reader :name, :roster
+  def initialize(name)
+    @roster = Hash.new
+    @name = name
+  end
+  
+  # grade pointint to an array of name
+  
+  def add_student(name,grade)
+     self.roster[grade] ||= []
+    self.roster[grade] << name
+  end
+  
+  def grade(number)
+    self.roster[number] 
+  end
+    
+    
+    def sort
+      
+       self.roster.each do | grade, name|
+         name.sort!
+       end
+    end
+    
+end
