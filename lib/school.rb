b504b375292444b0de758d992d055ba1f9466a7c
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-23 20:00:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,3 @@
 # code here!
-class School
-  attr_accessor :roster, :student_name, :school_name
-  def initialize(name)
-    @name = name
-    @roster = Hash.new{|h,k|  h[k] = [] }
-  end
 
-  def add_student(student_name, grade)
-   
-   self.roster[grade] << name
-  end
-end
 
