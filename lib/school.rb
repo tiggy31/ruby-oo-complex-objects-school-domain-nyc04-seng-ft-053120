9f9aabc7efc0594a31eae6d40b7da7943e3a3ac0
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-23 20:11:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,3 @@
 # code here!
 
-class School(name)
-  
-  attr_reader :name, :roster
-  
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
-  
-  def add_student(name,grade)
-    self.roster[grade] << name
-    
-  end
-  
-  
-end
-  
+class School
